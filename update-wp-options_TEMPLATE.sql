f3a2e51e129715984265d3bb36d1f9dae8924c9f
Jack-Bonnell <jackebonnell@gmail.com>
2020-05-11 17:09:40 +1000

    RESET BRANCH

--- update-wp-options_TEMPLATE.sql
+++
@@ -1,6 +0,0 @@
-USE wordpress;
-
-UPDATE wp_options SET option_value='http://__IP_ADDR__' WHERE option_name IN ('siteurl', 'home');
-UPDATE wp_options SET option_value='__SITE_NAME__' WHERE option_name='blogname';
-UPDATE wp_options SET option_value='__SITE_DESCRIPTION__' WHERE option_name='blogdescription';
-
