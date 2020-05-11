f28d80b28c5a7ba7ad32b5fd27b3c05f05866e39
Jack-Bonnell <jackebonnell@gmail.com>
2020-05-11 16:50:20 +1000

    UPDATE update-wp-options

--- update-wp-options.sh
+++ update-wp-options.sh
@@ -12,17 +12,9 @@
 SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 DIR_NAME=$(basename $SCRIPTPATH)
 
-SITE_NAME="$($SCRIPTPATH/exec-query.sh "SELECT option_value FROM wp_options WHERE option_name='blogname'" 2> /dev/null | tail -1)"
-SITE_DESCRIPTION="$($SCRIPTPATH/exec-query.sh "SELECT option_value FROM wp_options WHERE option_name='blogdescription'" 2> /dev/null | tail -1)"
-
-#UPDATE wp_options SET option_value='__SITE_NAME__' WHERE option_name='blogname';
-#UPDATE wp_options SET option_value='__SITE_DESCRIPTION__' WHERE option_name='blogdescription';
-
-
-
-SITE_NAME=${1:-"$SITE_NAME"}
-SITE_DESCRIPTION=${2:-"$SITE_DESCRIPTION"}
-IP_ADDR=$(localhost)
+SITE_NAME=${1:-"My Wordpress Site"}
+SITE_DESCRIPTION=${2:-"Just another WordPress site"}
+IP_ADDR=$(./my-ip-addr.sh)
 DB_CONTAINER_NAME="${DIR_NAME,,}_db_1"
 
 echo "Setting wp_options in container $DB_CONTAINER_NAME ..."
@@ -30,6 +22,7 @@ echo "  blogname=$SITE_NAME"
 echo "  blogdescription=$SITE_DESCRIPTION"
 echo "  siteurl=http://$IP_ADDR"
 echo "  home=http://$IP_ADDR"
+#exit
 
 IP_ADDR_PATTERN='s/__IP_ADDR__/'$IP_ADDR'/g'
 SITE_NAME_PATTERN='s/__SITE_NAME__/'$SITE_NAME'/g'
@@ -39,5 +32,3 @@ sed -e "$IP_ADDR_PATTERN" -e "$SITE_NAME_PATTERN" -e "$SITE_DESCRIPTION_PATTERN"
 
 docker exec -i "${DB_CONTAINER_NAME}" sh -c 'exec mysql -uwordpress -pwordpress' < .update-wp-options.sql 2>&1 | grep -v "Using a password on the command line"
 rm -f .update-wp-options.sql
-$SHELL
-
