17211c49270d67ec164cf2759c923b6c5c491414
Jack-Bonnell <jackebonnell@gmail.com>
2020-05-28 14:29:51 +1000

    ADD Gallery Content
    ADD Gallery Content
    UPDATE wp-options-server.sh

--- update-wp-options-server.sh
+++ update-wp-options-server.sh
@@ -14,7 +14,7 @@ DIR_NAME=$(basename $SCRIPTPATH)
 
 SITE_NAME=${1:-"Townsville Jazz Club"}
 SITE_DESCRIPTION=${2:-" "}
-IP_ADDR=10.0.0.7
+IP_ADDR=$(./my-ip-addr.sh)
 DB_CONTAINER_NAME="${DIR_NAME,,}_db_1"
 
 echo "Setting wp_options in container $DB_CONTAINER_NAME ..."
